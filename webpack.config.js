afee604765df3cff070665b806296bc105e17a8c
fcsimao <fcsimao@gmail.com>
2023-04-09 23:49:20 -0300

    configurado o webpack e serverless

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const sLsw = require('serverless-webpack')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+    entry: sLsw.lib.entries,
+    target: 'node',
+    devtool: 'source-map',
+    externals: [nodeExternals()],
+    mode: sLsw.lib.webpack.isLocal ? 'development' : 'production',
+    optimization: {
+        minimize: false
+    },
+    performance: {
+        hints: false
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: __dirname,
+                exclude: /node_modules/
+            }
+        ]
+    }
+}                                                                                                              
