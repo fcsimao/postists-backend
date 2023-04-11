877cabd8ab215442094cc83288adcd61f252c1a8
fcsimao <fcsimao@gmail.com>
2023-04-10 21:19:27 -0300

    eslint adicionado

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,25 @@ const sLsw = require('serverless-webpack')
 const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
-    entry: sLsw.lib.entries,
-    target: 'node',
-    devtool: 'source-map',
-    externals: [nodeExternals()],
-    mode: sLsw.lib.webpack.isLocal ? 'development' : 'production',
-    optimization: {
-        minimize: false
-    },
-    performance: {
-        hints: false
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                include: __dirname,
-                exclude: /node_modules/
-            }
-        ]
-    }
-}                                                                                                              
+  entry: sLsw.lib.entries,
+  target: 'node',
+  devtool: 'source-map',
+  externals: [nodeExternals()],
+  mode: sLsw.lib.webpack.isLocal ? 'development' : 'production',
+  optimization: {
+    minimize: false,
+  },
+  performance: {
+    hints: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: __dirname,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+}
